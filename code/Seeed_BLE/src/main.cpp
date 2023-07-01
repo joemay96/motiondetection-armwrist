2fbfbf9304da29b6e72b9fbe0243c1e76f356af0
joemay96 <little.joe96@gmx.de>
2023-07-02 00:50:39 +0200

    adding both BLE project boilerplates for esp32 and seeed onto platformio

--- code/Seeed_BLE/src/main.cpp
+++ code/Seeed_BLE/src/main.cpp
@@ -2,9 +2,10 @@
  * Therefore every device which supports BLE can be connected to the Seeed microcontroller and read out the IMU data that gets boardcasted.
  */
 
+#include <Arduino.h>
 #include <ArduinoBLE.h>
 
-// Weiß nicht ob die UUID hier random ist oder nicht, falls ich eine neue benötige: https://www.uuidgenerator.net/
+// Weiß nicht ob die UUID hier random ist oder nicht
 BLEService imuService("19B10000-E8F2-537E-4F6C-D104768A1214"); // Bluetooth® Low Energy LED Service
 // Bluetooth® Low Energy LED Switch Characteristic - custom 128-bit UUID, read and writable by central
 // 20 indicates the maximum length of data you can send.
@@ -14,13 +15,16 @@ BLEStringCharacteristic imuCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214"
 
 int last_val = 0;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   // begin initialization
-  if (!BLE.begin()) {
+  if (!BLE.begin())
+  {
     Serial.println("starting Seeed BLE failed!");
-    while (1);
+    while (1)
+      ;
   }
 
   // set advertised local name and service UUID:
@@ -38,26 +42,30 @@ void setup() {
   Serial.println("Seeed BLE ready");
 }
 
-void loop() {
+void loop()
+{
   // listen for Bluetooth® Low Energy peripherals to connect:
   BLEDevice central = BLE.central();
 
   // if a device is connected:
-  if (central) {
+  if (central)
+  {
     Serial.print("Connected to device: ");
     // print the device's MAC address:
     Serial.println(central.address());
 
     // while the device is connected send IMU data
-    while (central.connected()){
-       // TODO: get IMU data
-       if (true) { // check for new data
-          imuCharacteristic.value("Hello world!");
-        }
+    while (central.connected())
+    {
+      // TODO: get IMU data
+      if (true)
+      { // check for new data
+        imuCharacteristic.value("Hello world!");
+      }
     }
 
     // when the central disconnects, print it out:
-      Serial.print(F("Device disconnected: "));
-      Serial.println(central.address());
-    }
+    Serial.print(F("Device disconnected: "));
+    Serial.println(central.address());
+  }
 }
